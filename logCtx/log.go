f004262cb81667189d5d22012df25135f7d42055
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2025-08-23 17:36:21 +0800

    feat: added basic ctx log functionality

---
+++ logCtx/log.go
@@ -0,0 +1,35 @@
+package logCtx
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"bamboo_lite_common_sdk/env"
+)
+
+func formatString(fmtString string, args ...interface{}) string {
+	return fmt.Sprintf(fmtString, args...)
+}
+
+func getBaseArgs(ctx context.Context) []interface{} {
+	var args []interface{}
+	args = append(args, []string{"PSM", env.PSM()})
+	return args
+}
+
+func Info(ctx context.Context, fmtString string, args ...interface{}) {
+	slog.Info(formatString(fmtString, args...), getBaseArgs(ctx)...)
+}
+
+func Warn(ctx context.Context, fmtString string, args ...interface{}) {
+	slog.Warn(formatString(fmtString, args...), getBaseArgs(ctx)...)
+}
+
+func Error(ctx context.Context, fmtString string, args ...interface{}) {
+	slog.Error(formatString(fmtString, args...), getBaseArgs(ctx)...)
+}
+
+func Debug(ctx context.Context, fmtString string, args ...interface{}) {
+	slog.Debug(formatString(fmtString, args...), getBaseArgs(ctx)...)
+}
